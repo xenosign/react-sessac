4a718013af12b81be50295fa521e1cf254928397
xenosign <xenosign@naver.com>
2023-01-19 19:13:35 +0900

    2023 0119 수업

---
+++ mbti-app-2nd/src/App.js
@@ -0,0 +1,37 @@
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import GlobalStyle from './components/GlobalStyle';
+import Mbti from './components/Mbti';
+import Show from './components/Show';
+import Start from './components/Start';
+
+const Main = styled.main`
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 500px;
+  padding: 0 35px;
+  margin: auto;
+  text-align: center;
+`;
+
+function App() {
+  const page = useSelector((state) => state.mbti.page);
+  const survey = useSelector((state) => state.mbti.survey);
+
+  return (
+    <>
+      <GlobalStyle />
+      <Main>
+        {page === 0 ? (
+          <Start />
+        ) : page !== survey.length + 1 ? (
+          <Mbti />
+        ) : (
+          <Show />
+        )}
+      </Main>
+    </>
+  );
+}
+
+export default App;
